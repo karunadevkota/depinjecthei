42b0f9bcdb6c11be868aa036733065d6ffafdd66
Karuna Devkota <90544687+karunadevkota@users.noreply.github.com>
2023-07-26 08:05:02 +0545

    Add files via upload

---
+++ ChangePasswordViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.Models
+{
+    public class ChangePasswordViewModel
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Current Password")]
+        public string CurrentPassword { get; set; }
+        [Required]
+        [DataType (DataType.Password)]
+        [Display(Name = "New Password")]
+        public string NewPassword { get; set;}
+        [Required]
+        [DataType (DataType.Password)]
+        [Display(Name = "Confirm New Password")]
+        [Compare("NewPassword" , ErrorMessage = " Your New Password and Confirm PAssword do not match")]
+        public string ConfirmPassword { get; set;}
+    }
+}
