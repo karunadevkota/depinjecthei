8b832dc3c4b9cc42579d5fd33408c5b32a0b6609
Karuna Devkota <90544687+karunadevkota@users.noreply.github.com>
2023-07-26 08:04:22 +0545

    Add files via upload

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CMS.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+                
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseNpgsql("connection_string", b => b.MigrationsAssembly("CMS.Data"));
+            }
+        }
+
+
+    }
+}
+
+
+
+//  public DbSet<IdentityRole> Roles { get; set; }
